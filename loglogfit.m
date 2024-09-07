f8c68c453759942d1f46944c4afda55d69731ea6
普罗列塔利亚 <709258220@qq.com>
2024-09-07 17:48:12 +0800

    Add files via upload

--- loglogfit.m
+++ loglogfit.m
@@ -1,4 +1,4 @@
-function [Z] = loglogfit(X,Y)
+function [Z,W] = loglogfit(X,Y,I)
 
     if(any(X)<=0 || any(Y)<=0)
         error("log(X)和log(Y)需要X和Y>0");
@@ -6,10 +6,21 @@ function [Z] = loglogfit(X,Y)
     if(~isvector(X) || ~isvector(Y))
         error("X和Y应当是向量");
     end
+    if(size(X)~=size(Y))
+        error("X和Y应当拥有相同的size");
+    end
+    if(size(I)~=size(X))
+        error("I应当和X拥有相同的size");
+    end
+
+    % 选中用于拟合的数据点
+    validX = X(I);
+    validY = Y(I);
 
 
-    Coeff = polyfit(log(X),log(Y),1);
+    Coeff = polyfit(log(validX),log(validY),1);
     Z = polyval(Coeff,log(X));
+    W = Z-Y;
 
     if(nargout==0)
         figure;
